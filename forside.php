759ca4015c839eb24e1e8bfdd63f49395e5dc7c4
Smedeen <55687178+Smedeen@users.noreply.github.com>
2019-12-17 10:57:47 +0100

    starter forfra på github
    hello world

---
+++ forside.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Min portfolio</title>
+        <link rel="stylesheet"href="style.css">
+        
+         <nav>
+            <?php
+            include "nav bar.inc";
+            ?>
+        </nav>
+        
+    </head>
+    
+    <body>
+        
+<section id="mitgrid">
+    
+            
+        <section id="main">
+        </section>
+        
+        <section id="om">
+            <h2>Herunder kan man se overemnerne, som jeg har lært i mit første semester på Eadania Skive MMD.</h2>
+        </section> 
+        
+        <section id="program">
+            <a href=program.html><h1>Programmering</h1></a>
+        </section>
+        
+        <section id="design">
+            <a href="https://www.retrogames.cz/play_085-NES.php"><h1>Design</h1></a>
+        </section>
+        
+        <section id="webudvikling">
+            <a href="https://www.retrogames.cz/play_1223-NES.php"><h1>Webudvikling</h1></a>
+        </section>
+        
+        <section id="struktur">
+            <a href=https://www.retrogames.cz/play_400-NES.php>
+            <h1>Struktur</h1></a>
+        </section>
+        
+        <footer>
+            <p>&copy; Maj Smedemark - portfolio</p>
+        </footer>
+    
+    
+</section>
+        
+    </body>
+</html>
